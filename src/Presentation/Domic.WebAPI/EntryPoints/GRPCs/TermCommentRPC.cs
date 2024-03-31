d3f9424d3cbd3bd644fcc39921c4826695ce7c21
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-03-31 11:52:08 +0330

    added term_comment rpc controller

---
+++ src/Presentation/Domic.WebAPI/EntryPoints/GRPCs/TermCommentRPC.cs
@@ -0,0 +1,16 @@
+﻿using Domic.Core.TermComment.Grpc;
+using Domic.Core.UseCase.Contracts.Interfaces;
+
+namespace Domic.WebAPI.EntryPoints.GRPCs;
+
+public class TermCommentRPC : TermCommentService.TermCommentServiceBase
+{
+    private readonly IMediator      _mediator;
+    private readonly IConfiguration _configuration;
+
+    public TermCommentRPC(IMediator mediator, IConfiguration configuration)
+    {
+        _mediator      = mediator;
+        _configuration = configuration;
+    }
+}
